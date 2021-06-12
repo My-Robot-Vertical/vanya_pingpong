37c39be2134ddde8af7c8dfeadb0a224a415fa92
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-12 12:53:42 +0300

    first add

---
+++ VANYA.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from random import * # x0 = randint(0, 100)
+from copy import deepcopy
+#import time
+#from time import sleep
+from time import time
+
+out_window = Tk()
+out_window.title('BARS 1.0')
+    
+    
+'''
+    out_window = Tk()
+    out_window.title('BARS 1.0')  # demonstration
+    #window.geometry('1000x1000')
+    #canvas = Canvas(out_window,width=9*ras,height=9*ras)
+    canvas = Canvas(out_window,width=9*ras,height=10*ras,bg="grey") # ,cursor="pencil"
+    #canvas.create_rectangle(ras*4,ras*9.2,ras*5,ras*9.8,fill="grey")
+    #canvas.create_text(ras*7,ras*9.5,text="you must eat",font="Verdana 10",justify=CENTER,fill="black")
+    #canvas.create_oval([x*ras+ras/2+ras/10,(y-1)*ras+ras/2*3+ras/10],[x*ras+ras/2*3-ras/10,(y-1)*ras+ras/2*5-ras/10],fill="white", outline="black")
+
+        
+    
+        
+    
+    def click(event):
+        pass
+    
+    canvas.bind("<Button-1>", click)
+
+    canvas.pack()
+    out_window.mainloop()
+    #in_window.mainloop()
+'''
---
+++ VANYA_2.py
@@ -0,0 +1,6 @@
+
+print("Hello!")
+
+if __name__=="__main__":
+    print("er;jjsegjesrlgerslgjer;lgnf;dl")
+    
