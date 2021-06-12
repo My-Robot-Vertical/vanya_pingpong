feb4924cbc412b5d799d27795d29399de2046a3d
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-12 19:13:38 +0300

    Update main.py

--- Egor/main.py
+++ Egor/main.py
@@ -1,11 +1,8 @@
 from tkinter import *
 from random import * # x0 = randint(0, 100)
 from copy import deepcopy
-#import time
-from time import sleep
-from time import time
-
-from math import sin, cos, radians
+from time import sleep,time
+from math import sin, cos, radians, degrees, atan2
 
 AREA_X = 300
 AREA_Y = 300
@@ -13,6 +10,19 @@ AREA_Y = 300
 out_window = Tk()
 out_window.title('pingpong')
 canvas = Canvas(out_window,width=AREA_X,height=AREA_Y,bg="white")
+
+flag_window = 1
+def off_window():
+    global flag_window
+    flag_window = 0
+    
+def click(event):
+    off_window()
+    out_window.destroy()
+    
+canvas.bind("<Button-1>", click)
+canvas.pack()
+
 
 class Vector:
     
@@ -26,18 +36,44 @@ class Vector:
         self.fps = 100
         self.x_save = 0
         self.y_save = 0
+        self.color = "white"
+        self.g = 0.7
         
     def draw(self):
-        canvas.delete("all")
-        canvas.create_oval( [self.x-self.radius,self.y-self.radius], [self.x+self.radius,self.y+self.radius], fill="white", outline="black")
-        out_window.update()
+        try:
+            canvas.delete("all")
+            canvas.create_oval( [self.x-self.radius,self.y-self.radius], [self.x+self.radius,self.y+self.radius], fill=self.color, outline="black")
+            out_window.update()
+        except TclError:
+            off_window()
         
     def delay(self):
         sleep(1/self.fps)
         
     def move(self):
+        while (self.angle<0): self.angle+=360
+        while (self.angle>360): self.angle-=360
         self.x-=self.speed*sin(radians(self.angle))
         self.y+=self.speed*cos(radians(self.angle))
+        
+    def move_physics(self):
+        # устранение критических угловых значений
+        while (self.angle<0): self.angle+=360
+        while (self.angle>360): self.angle-=360
+        # просчет прямолинейного движения
+        dx = self.speed*sin(radians(self.angle))
+        dy = self.speed*cos(radians(self.angle))
+        # добавления силы тяжести
+        dy = dy+self.g #self.g
+        self.speed = (dy**2+dx**2)**(1/2)
+        self.angle = degrees(atan2(dx,dy))
+        # устранение критических угловых значений
+        while (self.angle<0): self.angle+=360
+        while (self.angle>360): self.angle-=360
+        # вычисление новых координат
+        self.x-=dx
+        self.y+=dy
+        #self.y-=1
         
     def save_coo(self):
         self.x_save = self.x
@@ -54,48 +90,43 @@ ball.y = 100
 ball.radius = 5
 ball.angle = 300
 ball.speed = 1
-ball.fps = 200
+ball.fps = 70
+ball.color = "red"
+ball.g = 0.01
     
 
 
-canvas.pack()
 
-while (1):
-    ball.save_coo()
-    ball.move()
-    if (ball.x-ball.radius>=0 and ball.y-ball.radius>=0 and ball.y+ball.radius<=AREA_Y and ball.x+ball.radius<=AREA_X):
-        pass
+while (flag_window):
+    if (ball.speed<0.01):
+        ball.speed = 0
+        ball.draw()
     else:
-        print("!!!")
-        if (ball.x-ball.radius<=0 or ball.x+ball.radius>=AREA_X): # врезались левой или правой частью
-            ball.angle = 360-ball.angle
-        if (ball.y-ball.radius<=0 or ball.y+ball.radius>=AREA_Y): # врезались верхней или нижней частью 
-            ball.angle = 540-ball.angle
-        ball.becup_coo()
-        ball.move()
-    #ball.x+=1
-    #ball.y+=1
-    ball.draw()
-    ball.delay()
-    #print(ball.x,ball.y)
-    
+        ball.save_coo()
+        ball.move_physics()
+        if (ball.x-ball.radius>=0 and ball.y-ball.radius>=0 and ball.y+ball.radius<=AREA_Y and ball.x+ball.radius<=AREA_X):
+            pass
+        else:
+            #print("!!!")
+            if (ball.x-ball.radius<=0 and 0<ball.angle<180 or ball.x+ball.radius>AREA_X and 180<ball.angle<360): # врезались левой или правой частью
+                ball.angle = 360-ball.angle
+            if (ball.y-ball.radius<=0 and 90<ball.angle<270 or ball.y+ball.radius>=AREA_Y and (ball.angle>0 or ball.angle<90)): # врезались верхней или нижней частью 
+                ball.angle = 540-ball.angle
+                if (ball.y+ball.radius>=AREA_Y): ball.speed = ball.speed/1.3
+            ball.becup_coo()
+            ball.move_physics()
+        ball.draw()
+        ball.delay()
+        #print(ball.x,ball.y,ball.angle,ball.speed) #event.x,event.y
+    #x = out_window.winfo_pointerx()
+    #y = out_window.winfo_pointery()
     
 
 
         
     
-        
-    '''
-    def click(event):
-        pass
-    
-    #canvas.create_rectangle(ras*4,ras*9.2,ras*5,ras*9.8,fill="grey")
-    #canvas.create_text(ras*7,ras*9.5,text="you must eat",font="Verdana 10",justify=CENTER,fill="black")
-    
-    canvas.bind("<Button-1>", click)
 
-    canvas.pack()
-    out_window.mainloop()
-    #in_window.mainloop()
-    '''
-out_window.mainloop()
+    
+#canvas.create_rectangle(ras*4,ras*9.2,ras*5,ras*9.8,fill="grey")
+#canvas.create_text(ras*7,ras*9.5,text="you must eat",font="Verdana 10",justify=CENTER,fill="black")
+#out_window.mainloop()
